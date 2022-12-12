61c8dd379240c4c5d109e5627b1cffa262f8530b
Umut Fidan <contact.umutfidan@gmail.com>
2022-12-12 19:41:39 +0300

    development branch

--- app/src/main/java/com/mergenc/mobilprogramlamaproje/MainActivity.kt
+++ app/src/main/java/com/mergenc/mobilprogramlamaproje/MainActivity.kt
@@ -3,6 +3,10 @@ package com.mergenc.mobilprogramlamaproje
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
+
+//uuuuuu
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
